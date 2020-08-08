95f39b2ca5a896dd4a0d5ad46d3cde8a11634a53
Tisdale511 <tisdalefry@gmail.com>
2020-08-08 16:58:10 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,11 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+// import ImageSlider from './ImageSlider';
+
+class Bomb extends React.Component{
+    render(){
+        return(
+           <Bomb intialCount='secondsLeft' /> 
+        )
+    }
+}
